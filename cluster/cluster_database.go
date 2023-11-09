ba8a13784386d45efcc9f4f629ca047ce119baa8
xinshuai.zhang <xss2619@163.com>
2023-11-09 21:33:41 +0800

    完善cluster_database

--- cluster/cluster_database.go
+++ cluster/cluster_database.go
@@ -1,36 +1,54 @@
+// Package cluster provides a server side cluster which is transparent to client. You can connect to any node in the cluster to access all data in the cluster
 package cluster
 
 import (
 	pool "github.com/jolestar/go-commons-pool/v2"
-	"go-redis/interface/database"
+	"go-redis/config"
+	"go-redis/database"
+	databaseface "go-redis/interface/database"
 	"go-redis/interface/resp"
 	"go-redis/lib/consistenthash"
 )
 
+// ClusterDatabase represents a node of godis cluster
+// it holds part of data and coordinates other nodes to finish transactions
 type ClusterDatabase struct {
 	self string
 
-	node       []string
-	peerPicker *consistenthash.NodeMap
-	// 保存多个连接池
+	nodes          []string
+	peerPicker     *consistenthash.NodeMap
 	peerConnection map[string]*pool.ObjectPool
-	db             database.Database
+	db             databaseface.Database
 }
 
+// MakeClusterDatabase creates and starts a node of cluster
 func MakeClusterDatabase() *ClusterDatabase {
+	cluster := &ClusterDatabase{
+		self: config.Properties.Self,
 
-}
-func (c *ClusterDatabase) Exec(client resp.Connection, args [][]byte) resp.Reply {
-	//TODO implement me
-	panic("implement me")
+		db:             database.NewStandaloneDatabase(),
+		peerPicker:     consistenthash.NewNodeMap(nil),
+		peerConnection: make(map[string]*pool.ObjectPool),
+	}
+	nodes := make([]string, 0, len(config.Properties.Peers)+1)
+	for _, peer := range config.Properties.Peers {
+		nodes = append(nodes, peer)
+	}
+	nodes = append(nodes, config.Properties.Self)
+	cluster.peerPicker.AddNode(nodes...)
+	cluster.nodes = nodes
+	return cluster
 }
 
-func (c *ClusterDatabase) Close() {
-	//TODO implement me
-	panic("implement me")
+// CmdFunc represents the handler of a redis command
+type CmdFunc func(cluster *ClusterDatabase, c resp.Connection, cmdAndArgs [][]byte) resp.Reply
+
+// Close stops current node of cluster
+func (cluster *ClusterDatabase) Close() {
+	cluster.db.Close()
 }
 
-func (c *ClusterDatabase) AfterClientClose(conn resp.Connection) {
-	//TODO implement me
-	panic("implement me")
+// AfterClientClose does some clean after client close connection
+func (cluster *ClusterDatabase) AfterClientClose(c resp.Connection) {
+	cluster.db.AfterClientClose(c)
 }
